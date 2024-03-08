de2c7c4924c05d505042687feafb92e69a181fdf
Thiago Lopes <thiago.lopes@scaleup.com.br>
2024-03-08 20:47:05 -0300

    Added empty state on Favorites & Cart screen

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,12 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
-import AppLayout from '@src/components/AppLayout'
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome6'
+import { Product } from '@Data/types'
+import { colors } from '@Theme/colors'
+import AppLayout from '@ComponentsAppLayout'
 
 export default function CartScreen() {
+  const cart: Product[] = []
+
+  function deleteAllOnCart() {
+    console.log('delete all from cart')
+  }
+
   return (
-    <AppLayout fullHeight contentPadding>
-      <Text>Cart</Text>
+    <AppLayout fullHeight>
+      <TouchableOpacity onPress={deleteAllOnCart} style={styles.deleteAllBtn}>
+        <Text style={styles.deleteAllLabel}>Delete All from Cart</Text>
+      </TouchableOpacity>
+
+      {cart.length ? (
+        cart.map((product) => <Text key={product.id}>{product.name}</Text>)
+      ) : (
+        <View style={styles.emptyCart}>
+          <Icon
+            solid
+            name="bag-shopping"
+            size={58}
+            style={{ color: colors.blackCardBg }}
+          />
+
+          <Text style={styles.emptyMessage}>
+            The products you add to your cart will appear here
+          </Text>
+        </View>
+      )}
     </AppLayout>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  deleteAllBtn: {
+    width: '85%',
+    marginTop: 23,
+    alignSelf: 'center',
+    borderRadius: 20,
+    backgroundColor: colors.txtDarkColor,
+    paddingVertical: 12,
+    marginBottom: 20
+  },
+  deleteAllLabel: {
+    color: colors.whiteColor,
+    textAlign: 'center'
+  },
+  emptyCart: {
+    flex: 1,
+    rowGap: 12,
+    marginBottom: 80,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyMessage: {
+    fontSize: 14,
+    maxWidth: 200,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: colors.blackCardBg
+  }
+})
--- src/screens/FavoriteScreen.tsx
+++ src/screens/FavoriteScreen.tsx
@@ -1,12 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import AppLayout from '@src/components/AppLayout'
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome6'
+import { Product } from '@Data/types'
+import { colors } from '@Theme/colors'
+import AppLayout from '@ComponentsAppLayout'
 
 export default function FavoriteScreen() {
+  const favorites: Product[] = [] // allProducts.filter((product) => product.favorite)
+
+  function deleteAllFavorites() {
+    console.log('delete all favorites')
+  }
+
   return (
-    <AppLayout fullHeight contentPadding>
-      <Text>Favorites</Text>
+    <AppLayout fullHeight>
+      <TouchableOpacity
+        onPress={deleteAllFavorites}
+        style={styles.deleteAllBtn}
+      >
+        <Text style={styles.deleteAllLabel}>Delete All Favorites</Text>
+      </TouchableOpacity>
+
+      {favorites.length ? (
+        favorites.map((product) => <Text key={product.id}>{product.name}</Text>)
+      ) : (
+        <View style={styles.emptyFavorites}>
+          <View style={styles.emptyIcon}>
+            <Icon
+              solid
+              name="heart"
+              size={32}
+              style={{ color: colors.containerColor }}
+            />
+          </View>
+
+          <Text style={styles.emptyMessage}>
+            The products you favorite will appear here
+          </Text>
+        </View>
+      )}
     </AppLayout>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  deleteAllBtn: {
+    width: '85%',
+    marginTop: 23,
+    alignSelf: 'center',
+    borderRadius: 20,
+    backgroundColor: colors.txtDarkColor,
+    paddingVertical: 12,
+    marginBottom: 20
+  },
+  deleteAllLabel: {
+    color: colors.whiteColor,
+    textAlign: 'center'
+  },
+  emptyFavorites: {
+    flex: 1,
+    rowGap: 12,
+    marginBottom: 80,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyIcon: {
+    padding: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.blackCardBg,
+    borderRadius: 50
+  },
+  emptyMessage: {
+    fontSize: 14,
+    maxWidth: 200,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: colors.blackCardBg
+  }
+})
