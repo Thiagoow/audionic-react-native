4d111b56495209498c3cd16cae2f7ac6b28e7c4f
Thiago Lopes <thiago.lopes@scaleup.com.br>
2024-03-15 18:14:51 -0300

    Created & added OrderCard component

---
+++ src/components/OrderCard.tsx
@@ -0,0 +1,114 @@
+import React, { useMemo } from 'react'
+import { StyleSheet, Text, View, ViewProps } from 'react-native'
+import { Order } from '@Data/types'
+import { colors } from '@Theme/colors'
+import formatPrice from '@Utils/formatPrice'
+
+type OrderCardProps = Order & ViewProps
+
+const OrderCard = ({ id, date, products, total, ...rest }: OrderCardProps) => {
+  return useMemo(
+    () => (
+      <View style={styles.container} {...rest}>
+        <Text style={styles.title}>
+          Order Number: <Text style={styles.orderNumber}>{'' + id}</Text>
+        </Text>
+
+        <View style={styles.orderInfo}>
+          <View style={styles.infoTexts}>
+            <Text style={styles.infoLabel}>Payment Type:</Text>
+            <Text style={styles.infoLabel}>Credit Card (1x)</Text>
+          </View>
+
+          <View style={styles.infoTexts}>
+            <Text style={styles.infoLabel}>{date}</Text>
+            <Text style={styles.infoTotal}>{formatPrice(total)}</Text>
+          </View>
+        </View>
+
+        <View style={styles.productsContainer}>
+          {products.map((product) => (
+            <View style={styles.product} key={product.id}>
+              <View style={styles.infoTexts}>
+                <Text key={product.id} style={styles.productName}>
+                  {product.name}
+                </Text>
+
+                <Text key={product.id} style={styles.productLabel}>
+                  {formatPrice(product.colors[0].price)}
+                </Text>
+              </View>
+
+              <View style={styles.infoTexts}>
+                <Text key={product.id} style={styles.productLabel}>
+                  Color: {product.colors[0].color}
+                </Text>
+
+                <Text key={product.id} style={styles.productLabel}>
+                  Quantity: {product.colors[0].quantity}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </View>
+    ),
+    [id, date, Object.keys(products), total, Object.keys(rest)]
+  )
+}
+
+export default OrderCard
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 21,
+    alignItems: 'center',
+    backgroundColor: colors.whiteColor,
+    borderRadius: 20,
+    marginBottom: 10
+  },
+  title: {
+    fontFamily: 'Poppins_600SemiBold',
+    fontSize: 12,
+    marginBottom: 26
+  },
+  orderNumber: {
+    color: colors.primaryColor
+  },
+  orderInfo: {
+    rowGap: 10,
+    marginBottom: 24
+  },
+  infoTexts: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  infoLabel: {
+    fontFamily: 'Poppins_600SemiBold',
+    color: colors.greyColor,
+    fontSize: 12
+  },
+  infoTotal: {
+    fontFamily: 'Poppins_600SemiBold',
+    color: colors.primaryColor,
+    fontSize: 12
+  },
+  productsContainer: {
+    width: '100%',
+    rowGap: 16
+  },
+  product: {
+    rowGap: 10
+  },
+  productName: {
+    fontFamily: 'Poppins_600SemiBold',
+    color: colors.blackColor,
+    fontSize: 12
+  },
+  productLabel: {
+    fontFamily: 'Poppins_400Regular',
+    color: colors.blackColor,
+    fontSize: 12
+  }
+})
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,18 +1,105 @@
 import { useContext } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import AppLayout from '@src/components/AppLayout'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome6'
+import { colors } from '@Theme/colors'
+import AppLayout from '@Components/AppLayout'
+import OrderCard from '@Components/OrderCard'
 import { GlobalContext } from '@Context/GlobalState'
+import { Order } from '@Data/types'
 
 export default function ProfileScreen() {
   const { orders } = useContext(GlobalContext)
 
-  console.log(orders)
+  function toggleSortByDate() {
+    console.log('Sort by date')
+    console.log(orders)
+  }
 
   return (
-    <AppLayout fullHeight contentPadding>
-      <Text>{orders.length}</Text>
+    <AppLayout fullHeight>
+      <TouchableOpacity
+        disabled={!orders.length}
+        onPress={toggleSortByDate}
+        style={[
+          styles.sortBtn,
+          {
+            backgroundColor: orders.length
+              ? colors.txtDarkColor
+              : colors.blackCardBg
+          }
+        ]}
+      >
+        <Text style={styles.sortLabel}>Sort by: </Text>
+      </TouchableOpacity>
+
+      {orders.length ? (
+        <ScrollView
+          contentContainerStyle={styles.itemsScroll}
+          showsVerticalScrollIndicator={false}
+        >
+          {orders.map((order: Order) => (
+            <OrderCard {...order} key={order.id} />
+          ))}
+        </ScrollView>
+      ) : (
+        <View style={styles.emptyOrders}>
+          <Icon
+            solid
+            name="boxes-packing"
+            size={58}
+            style={{ color: colors.blackCardBg }}
+          />
+
+          <Text style={styles.emptyMessage}>
+            The orders you made will appear here
+          </Text>
+        </View>
+      )}
     </AppLayout>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  sortBtn: {
+    width: '85%',
+    marginTop: 23,
+    alignSelf: 'center',
+    borderRadius: 20,
+    backgroundColor: colors.txtDarkColor,
+    paddingVertical: 12,
+    marginBottom: 30
+  },
+  sortLabel: {
+    color: colors.whiteColor,
+    fontFamily: 'Poppins_400Regular',
+    textAlign: 'center'
+  },
+  emptyOrders: {
+    flex: 1,
+    rowGap: 12,
+    marginBottom: 80,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyMessage: {
+    fontSize: 14,
+    maxWidth: 200,
+    textAlign: 'center',
+    fontFamily: 'Poppins_600SemiBold',
+    color: colors.blackCardBg
+  },
+  itemsScroll: {
+    rowGap: 30,
+    width: '100%',
+    paddingBottom: 23,
+    paddingHorizontal: 35,
+    alignSelf: 'center'
+  }
+})
